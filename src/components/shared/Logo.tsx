842d7fcb763f79c642daa41de8bf0d227e771d4b
tomi <tomi@tomi-2.local>
2025-03-18 13:33:12 +0900

    feat: update logo and manifest text from Magic Resume to We CV

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -35,7 +35,7 @@ const Logo: React.FC<LogoProps> = ({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
-      aria-label="Magic Resume Logo"
+      aria-label="We CV Logo"
       onClick={onClick}
     >
       <defs>
